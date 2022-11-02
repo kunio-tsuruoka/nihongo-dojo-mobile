af9cf3d4d674c3ceda6383f7b1e710da23c0bd0e
kunio tsuruoka <geekywakaba@gmail.com>
2022-11-02 19:17:40 +0900

    emotion11実装

---
+++ src/types/emotion.d.ts
@@ -0,0 +1,42 @@
+import '@emotion/react';
+
+declare module '@emotion/react' {
+  export interface Theme {
+    color: {
+      primary0: string;
+      primary1: string;
+      primary2: string;
+      primary3: string;
+      primary4: string;
+      primary5: string;
+      primary6: string;
+      primary7: string;
+      primary8: string;
+      secondary0: string;
+      secondary1: string;
+      secondary2: string;
+      secondary3: string;
+      secondary4: string;
+      secondary5: string;
+      secondary6: string;
+      tertiary0: string;
+      tertiary1: string;
+      tertiary2: string;
+      text0: string;
+      text1: string;
+      text2: string;
+      text3: string;
+      text4: string;
+      bg0: string;
+      bg1: string;
+      bg2: string;
+    };
+    size: {
+      maxWidth: string;
+    };
+    mediaQuery: {
+      sp: string;
+      ipad: string;
+    };
+  }
+}
